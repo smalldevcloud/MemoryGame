cad9622571162696fd3747a01d7abec82a7be58d
Alex Ch <alexch@192.168.2.75>
2025-02-03 20:14:41 +0300

    fake launch screen added

---
+++ MemoryGame/MemoryGame/Views/FakeLaunchScreen/FakeLSController.swift
@@ -0,0 +1,39 @@
+//
+//  FakeLSController.swift
+//  MemoryGame
+//
+//  Created by Alex Ch on 3.02.25.
+//
+
+import UIKit
+
+class FakeLSController: UIViewController {
+
+    var fireImageView = UIImageView()
+    let fireImage = UIImage(named: "fire")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fireImageView.image = fireImage
+        fireImageView.contentMode = .scaleAspectFit
+        view.addSubview(fireImageView)
+        
+        self.fireImageView.frame = CGRect(x: 0, y: 55, width: 400, height: 400)
+
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       options: [.curveEaseInOut, .autoreverse, .repeat]) {
+            UIView.modifyAnimations(withRepeatCount: 2, autoreverses: true) {
+                self.fireImageView.frame = CGRect(x: 0, y: 400, width: 400, height: 400);
+            }
+        } completion: { _ in
+            let vc = MainMenuViewController()
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: false) {
+                print("kek")
+            }
+        }
+    }
+
+
+}
---
+++ MemoryGame/MemoryGame/Views/MainMenu/MainMenuViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainMenuViewController.swift
+//  MemoryGame
+//
+//  Created by Alex Ch on 3.02.25.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
